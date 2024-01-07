b2c5f56c5026704490ddf1ea6aab3b3c167bdcd8
Geziel Junio <gezielsilva@pactosolucoes.com.br>
2024-01-06 22:25:17 -0300

    assets changed

---
+++ assets/tiles/asset3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="asset3" tilewidth="18" tileheight="18" tilecount="12" columns="12">
+ <image source="../images/tilesets/dungeon-assets2.png" width="222" height="18"/>
+</tileset>
